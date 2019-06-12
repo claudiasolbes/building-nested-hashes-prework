f5c6da78090cba29d191034325515ea3d550bb81
Claudia Solbes <cssolbes@hotmail.com>
2019-06-12 02:51:12 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -7,7 +7,7 @@ def third_challenge
         },
       :matriarch => {
         :name => "Lady Montague",
-        :age of "54"
+        :age => "54"
         },
       :hero => {
         #your key/value pairs here
