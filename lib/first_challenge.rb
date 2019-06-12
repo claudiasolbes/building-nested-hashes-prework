015e3d236adf3fb48a0ada0b7ba91bec103a7274
Claudia Solbes <cssolbes@hotmail.com>
2019-06-12 02:47:34 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,6 +1,12 @@
 def first_challenge
    epic_tragedy = {
-     :montague => {:patriarch => {}, :matriarch => {}, :hero => {}, },
-     :capulet => {}
+     :montague => {
+       :patriarch => {},
+       :matriarch => {},
+       :hero => {},
+       :hero_friends => []
+       
+     },
+     :capulet => {:patriarch => {}, :matriarch => {}, :heroine => {}, :heroine_friends => []}
    }
 end
